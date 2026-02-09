f4458b3d1b40a6e87402590bfc033b97b7927f5f
William Jamieson <william@jamieson.phd>
2026-02-09 16:13:27 -0500

    Refactor Fits related code into a mixin class for better code organization

--- gwcs/wcs/__init__.py
+++ gwcs/wcs/__init__.py
@@ -1,9 +1,11 @@
 from ._exception import GwcsBoundingBoxWarning, NoConvergence
+from ._fits import FitsMixin
 from ._step import Step
 from ._wcs import WCS
 
 __all__ = [
     "WCS",
+    "FitsMixin",
     "GwcsBoundingBoxWarning",
     "NoConvergence",
     "Step",
